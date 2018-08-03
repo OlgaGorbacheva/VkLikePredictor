9107f5f865090c9b6a9c8800ba8d09622f5b4c03
Olga Gorbacheva <olga.gorbacheva@phystech.edu>
2018-08-03 18:21:31 +0300

    Logging, policy politics and collecting functuin added

--- get_info.py
+++ get_info.py
@@ -1,14 +1,22 @@
 import json
 import time
+import csv
+import logging
 
 import urllib.request
 import urllib.parse
 
-def get_token(token_file_name='/home/common_swift/programming/likepredictor/access_token'):
+logging.basicConfig(filename='collectingdata.log',level=logging.INFO)
+
+def get_token(
+    token_file_name='/home/common_swift/programming/likepredictor/access_token'
+    ):
     with open(token_file_name, 'r') as f:
         return f.read().rstrip()
+    logging.info('Reached accsess tocken')
 
 def exec(method, values, access_token):
+    logging.info('Method {} started'.format(method))
     vk_api_url = 'https://api.vk.com/method/{}'
     values['version'] = '5.8'
     values['access_token'] = access_token
@@ -20,24 +28,38 @@ def exec(method, values, access_token):
         encoding = response.info().get_content_charset('utf-8')
         res = json.loads(the_page.decode(encoding))
         if 'error' in res:
-        	print(res['error']['error_msg'])
-        	raise 
+            logging.error(
+                'vk api error with msg: {}'.format(res['error']['error_msg'])
+            )
+            time.sleep(1)
+            return exec(method, values, access_token)
         return res['response']
 
 def get_user_info(access_token, user_id, photo_filename):
+    fields = [
+        'counters', 'crop_photo', 'followers_count',
+        'has_photo', 'photo_id', 'sex'
+    ]
     res = exec(
         'users.get', 
         {
             'user_ids': user_id,
-            'fields': 'counters,crop_photo,followers_count,has_photo,photo_id,relation,sex'
+            'fields': ','.join(fields)
         },
         access_token
     )
     res = res[0]
     if res['has_photo']:
-        urllib.request.urlretrieve(res['crop_photo']['photo']['src_small'], photo_filename)
+        urllib.request.urlretrieve(
+            res['crop_photo']['photo']['src_small'], 
+            photo_filename
+        )
+        logging.info(
+            'Photo {} is saved in {}'.format(res['photo_id'], photo_filename)
+        )
         return res
     else:
+        logging.info('User {} has no photo'.format(user_id))
         return None
 
 def get_photo_info(access_token, photo_id):
@@ -64,40 +86,57 @@ def get_group_users(access_token, group_id, count=1000):
     return res['users']
 
 def compound_info(user_info, photo_info):
-	counters = ['user_photos', 'videos', 'pages', 'groups', 'gifts', 'followers', 'photos', 'friends', 'audios']
-	info = {c: user_info['counters'][c] if c in user_info['counters'] else None for c in counters}
-	info['uid'] = user_info['uid']
-	info['sex'] = user_info['sex']
-	info['likes'] = photo_info['likes']['count']
-	info['timestamp'] = photo_info['created']
-	return info
+    counters = [
+        'user_photos', 'videos', 'pages', 
+        'groups', 'gifts', 'followers', 
+        'photos', 'friends', 'audios'
+    ]
+    info = {
+        c: user_info['counters'][c] if c in user_info['counters'] else None 
+        for c in counters
+    }
+    info['uid'] = user_info['uid']
+    info['sex'] = user_info['sex']
+    info['likes'] = photo_info['likes']['count']
+    info['timestamp'] = photo_info['created']
+    return info
 
 def get_full_user_info(access_token, user_id):
-	user_info = get_user_info(access_token, user_id, './photos/{}'.format(user_id))
-	if user_info is None:
-		return None
-	photo_info = get_photo_info(access_token, user_info['photo_id'])
-	return compound_info(user_info, photo_info)
+    logging.info('Getting user {} info'.format(user_id))
+    user_info = get_user_info(
+        access_token, user_id, './photos/{}'.format(user_id)
+    )
+    if user_info is None:
+        return None
+    photo_info = get_photo_info(access_token, user_info['photo_id'])
+    logging.info('Success getting info')
+    return compound_info(user_info, photo_info)
 
-def get_df(access_token, user_id_list):
-	data = []
-	for uid in user_id_list:
-		print(uid)
-		info = get_full_user_info(access_token, uid)
-		if info is not None:
-			data.append(info)
-		time.sleep(1)
-	print(data)
+def get_df(access_token, user_id_list, filename = 'data.csv'):
+    data = []
+    logging.info('Start collecting info')
+    for uid in user_id_list:
+        info = get_full_user_info(access_token, uid)
+        if info is not None:
+            data.append(info)
+    keys = data[0].keys()
+    logging.info('Start saving info')
+    with open(filename, 'w') as output:
+        dict_writer = csv.DictWriter(output, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(data)
+    logging.info('{} lines user\'s info is availible now'.format(len(data)))
+    return data
 
-def main():
+def collect():
     access_token = get_token()
     group_id = '34215577'
-    group_members = get_group_users(access_token, group_id, 5)
-    print(group_members)
-    get_df(access_token, group_members)
-    # uid = '176'
-    # info = get_user_info(access_token, uid, 'aa')
-    # print(info)
+    uids = []
+    logging.info('Start collecting user ids from vk.com/overhear')
+    for i in range(10):
+    	uids += get_group_users(access_token, group_id, offset=i*1000)
+    assert len(uids) == len(set(uids))
+    get_df(access_token, uids)
 
 if __name__ == '__main__':
-    main()
+    collect()
