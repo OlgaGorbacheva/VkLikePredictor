b5ca3f920ecbc65f85eae7507dab9042e40f814d
Olga Gorbacheva <olga.gorbacheva@phystech.edu>
2018-08-06 19:00:59 +0300

    Fixed photo saving problems

--- get_info.py
+++ get_info.py
@@ -2,15 +2,36 @@ import json
 import time
 import csv
 import logging
+import argparse
 
 import urllib.request
 import urllib.parse
+import os
 
-logging.basicConfig(format='%(levelname)s:%(message)s',level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s\t%(levelname)s:\t%(message)s',
+    level=logging.INFO
+)
 
-def get_token(
-    token_file_name='/home/common_swift/programming/likepredictor/access_token'
-    ):
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-n', '--number', help='Number of users', type=int, default=1000
+    )
+    parser.add_argument(
+        '--token', help='File with access_token', type=str,
+        default='access_token'
+    )
+    parser.add_argument('--gid', help='Group id', type=str, default='34215577')
+    parser.add_argument(
+        '--folder', help='Folder to save photos', default='photos'
+    )
+    parser.add_argument(
+        '-f', '--datafile', help='Csv file to text data', default='data.csv'
+    )
+    return parser.parse_args()
+
+def get_token(token_file_name):
     with open(token_file_name, 'r') as f:
         return f.read().rstrip()
     logging.info('Reached accsess tocken')
@@ -31,14 +52,24 @@ def exec(method, values, access_token):
             logging.error(
                 'vk api error with msg: {}'.format(res['error']['error_msg'])
             )
-            time.sleep(1)
-            return exec(method, values, access_token)
+            if res['error']['error_code'] == 6:
+                time.sleep(1)
+                return exec(method, values, access_token)
+            elif res['error']['error_code'] == 14:
+                values['captcha_sid'] = res['error']['captcha_sid']
+                logging.critical('You need enter captcha from this link')
+                logging.critical(res['error']['captcha_img'])
+                values['captcha_key'] = input('Input text from img: ')
+                logging.info('Try to resend response')
+                return exec(method, values, access_token)
+            else:
+                raise
         return res['response']
 
 def get_user_info(access_token, user_id, photo_filename):
     fields = [
         'counters', 'crop_photo', 'followers_count',
-        'has_photo', 'photo_id', 'sex'
+        'has_photo', 'photo_id', 'sex', 'photo_200'
     ]
     res = exec(
         'users.get', 
@@ -51,7 +82,7 @@ def get_user_info(access_token, user_id, photo_filename):
     res = res[0]
     if res['has_photo'] and 'crop_photo' in res:
         urllib.request.urlretrieve(
-            res['crop_photo']['photo']['src_small'], 
+            res['photo_200'], 
             photo_filename
         )
         logging.info(
@@ -102,10 +133,10 @@ def compound_info(user_info, photo_info):
     info['timestamp'] = photo_info['created']
     return info
 
-def get_full_user_info(access_token, user_id):
+def get_full_user_info(access_token, user_id, dirname):
     logging.info('Getting user {} info'.format(user_id))
     user_info = get_user_info(
-        access_token, user_id, './photos/{}'.format(user_id)
+        access_token, user_id, os.path.join(dirname, str(user_id))
     )
     if user_info is None:
         return None
@@ -113,31 +144,43 @@ def get_full_user_info(access_token, user_id):
     logging.info('Success getting info')
     return compound_info(user_info, photo_info)
 
-def get_df(access_token, user_id_list, filename = 'data.csv'):
+def get_df(access_token, user_id_list, filename, dirname):
     data = []
     logging.info('Start collecting info')
-    for uid in user_id_list:
-        info = get_full_user_info(access_token, uid)
-        if info is not None:
-            data.append(info)
-    keys = data[0].keys()
-    logging.info('Start saving info')
-    with open(filename, 'w') as output:
-        dict_writer = csv.DictWriter(output, keys)
-        dict_writer.writeheader()
-        dict_writer.writerows(data)
-    logging.info('{} lines user\'s info is availible now'.format(len(data)))
+    try:
+        for i, uid in enumerate(user_id_list):
+            logging.info('Iteration {} user {}'.format(i, uid))
+            info = get_full_user_info(access_token, uid, dirname)
+            if info is not None:
+                data.append(info)
+    except:
+        logging.critical('Some exception occured')
+        logging.critical('Saving uid list in file userlist_exception')
+        with open('userlist_exception', 'w') as output:
+            output.write(', '.join(str(e) for e in user_id_list))
+    finally:
+        keys = data[0].keys()
+        logging.info('Start saving info')
+        with open(filename, 'w') as output:
+            dict_writer = csv.DictWriter(output, keys)
+            dict_writer.writeheader()
+            dict_writer.writerows(data)
+        logging.info('{} lines user\'s info is availible now'.format(len(data)))
     return data
 
-def collect():
-    access_token = get_token()
-    group_id = '34215577'
+def collect(n, group_id, data_file, token_file, folder):
+    access_token = get_token(token_file)
     uids = []
     logging.info('Start collecting user ids from vk.com/overhear')
-    for i in range(10):
-    	uids += get_group_users(access_token, group_id, offset=i*1000)
+    for i in range(0, n, 1000):
+        uids += get_group_users(access_token, group_id, min(n - i, 1000), i)
     assert len(uids) == len(set(uids))
-    get_df(access_token, uids)
+    get_df(access_token, uids, data_file, folder)
 
 if __name__ == '__main__':
-    collect()
+    args = parse_args()
+    if args.folder[0] != '/':
+        args.folder = os.path.join(os.getcwd(), args.folder)
+    if not os.path.exists(args.folder):
+        os.mkdir(args.folder)
+    collect(args.number, args.gid, args.datafile, args.token, args.folder)
